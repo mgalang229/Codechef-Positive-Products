0c9374ca3e3da8937effbf41e77f30ff3ba5a1cd
Mark Jason Galang <51401355+mgalang229@users.noreply.github.com>
2022-04-14 11:38:30 +0800

    Create main.java

---
+++ main.java
@@ -0,0 +1,94 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) {	
+		FastScanner fs = new FastScanner();
+		PrintWriter out = new PrintWriter(System.out);
+		int T = 1;
+		T = fs.nextInt();
+		for(int tc = 0; tc < T; tc++) {
+			int n = fs.nextInt();
+			int[] a = fs.readArray(n);
+			long positive = 0, negative = 0;
+			for(int i = 0; i < n; i++) {
+				//don't include the zeroes
+				if(a[i] > 0) {
+					positive++;
+				}
+				else if(a[i] < 0) {
+					negative++;
+				}
+			}
+			//calculate the no. of distinct pairs
+			long ans = (positive * (positive - 1)) / 2;
+			ans += (negative * (negative - 1)) / 2;
+			out.println(ans);
+		}
+		out.close();
+	}
+	
+	static void sort(int[] a) {
+		ArrayList<Integer> arr = new ArrayList<>();
+		for(int x : a) {
+			arr.add(x);
+		}
+		Collections.sort(arr);
+		for(int i = 0; i < a.length; i++) {
+			a[i] = arr.get(i);
+		}
+	}
+		
+	static class FastScanner {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer("");
+		
+		String next() {
+			while(!st.hasMoreTokens()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		
+		int[] readArray(int n) {
+			int[] a = new int[n];
+			for(int i = 0; i < n; i++) {
+				a[i] = nextInt();
+			}
+			return a;
+		}
+		
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
